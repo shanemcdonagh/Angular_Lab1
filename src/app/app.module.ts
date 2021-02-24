059643465d500adaeaa75c1b6b2d49f77c5e81de
shanemcdonagh <shanemcdonagh72@gmail.com>
2021-02-24 15:16:49 +0000

    Lab One

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentaComponent } from './componenta/componenta.component';
+import { ComponentbComponent } from './componentb/componentb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentaComponent,
+    ComponentbComponent
   ],
   imports: [
     BrowserModule
